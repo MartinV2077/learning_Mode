ee8f4e95f33e0b5079e0dec1a9a6a99443dd0195
MartinVz <martinvz2077@gmail.com>
2022-04-18 20:37:14 -0500

    Second Learning from LINUX

---
+++ CursoJava/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+       
+        
+        var nombre = "Martin";
+        var edad = 44;
+        System.out.printf("Nombre: %s \nEdad: %d ", nombre, edad);
+    }
+}
