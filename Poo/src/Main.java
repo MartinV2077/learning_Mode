b1146629fcfbcc89e8feace3007c7b5c152f5697
MartinVz <martinvz2077@gmail.com>
2022-05-01 00:13:59 -0500

    Practica para POO en Java2

--- Poo/src/Main.java
+++ Poo/src/Main.java
@@ -1,18 +1,12 @@
 
+
+import Clases.Persona;
+
 public class Main {
 
    
     public static void main(String[] args) {
-        Persona persona1;
-        persona1 = new Persona();
-        persona1.nombre = "Martin";
-        persona1.edad = 44;
-        persona1.mostrarDatos(); 
-        
-        
-        Persona persona2 = new Persona();
-        persona2.nombre = "Jenny";
-        persona2.edad = 38;
-        persona2.mostrarDatos(); 
+       Persona persona1 = new Persona();
+       Persona persona2 = new Persona();
     }
 }
