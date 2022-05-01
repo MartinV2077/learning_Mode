abe390ab7bac3e671189572a53b46e2cea258fec
MartinVz <martinvz2077@gmail.com>
2022-04-30 23:41:27 -0500

    Practica para POO en Java

---
+++ Poo/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+        Persona persona1;
+        persona1 = new Persona();
+        persona1.nombre = "Martin";
+        persona1.edad = 44;
+        persona1.mostrarDatos(); 
+        
+        
+        Persona persona2 = new Persona();
+        persona2.nombre = "Jenny";
+        persona2.edad = 38;
+        persona2.mostrarDatos(); 
+    }
+}
---
+++ Poo/src/Persona.java
@@ -0,0 +1,9 @@
+
+public class Persona {
+   String nombre;
+   int edad;
+    void mostrarDatos (){
+    System.out.println("Nombre: "+ nombre); 
+    System.out.println("Edad: "+edad);
+    }
+}
